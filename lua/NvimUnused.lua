e0fcca7ba6e7684c4f35ef1146bf97d5909fde96
Inacio Klassmann <iklassman@lendingclub.com>
2023-11-23 08:27:05 -0300

    adding nvim-unused plugin

---
+++ lua/NvimUnused.lua
@@ -0,0 +1,9 @@
+local Unused = {}
+
+Unused.javaClass = function()
+	print("searching for unused java classes...")
+end
+
+-- vim.api.nvim_create_user_command("Unused", Unused.javaClass, {})
+
+return Unused
