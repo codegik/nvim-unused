ca9d2258d611ccb9484033b8ac7be07d1d6238c1
Inacio Klassmann <iklassman@lendingclub.com>
2023-11-23 17:15:24 -0300

    adding nvim-unused plugin

--- lua/NvimUnused.lua
+++ lua/NvimUnused.lua
@@ -1,18 +1,75 @@
+local function readFile(file)
+	local f = assert(io.open(file, "rb"))
+	local content = f:read("*all")
+	f:close()
+	return content
+end
+
 local function listFiles(root, ext)
 	local input = io.popen("find " .. root .. ' -type f |grep "\\.' .. ext .. '"')
+	local files = {}
 
 	for file in input:lines() do
-		local index = string.find(file, "[^\\/]*$")
-		local className = string.sub(file, index):gsub("." .. ext, "")
-		print("=> " .. className)
+		local index = file:find("[^\\/]*$")
+		local class = file:sub(index):gsub("." .. ext, "")
+		files[class] = file
 	end
 
 	input:close()
+
+	return files
+end
+
+local skipByContent = { "void main" }
+
+local function skipFileByContent(class, fileContent)
+	for i, skip in ipairs(skipByContent) do
+		if fileContent:find("\\" .. skip) then
+			return true
+		end
+	end
+
+	return false
 end
 
 local Unused = {}
 Unused.javaClass = function()
-	listFiles("../pocs/java-17-diamond", "java")
+	local files = listFiles("../pocs/java-17-diamond", "java")
+
+	for class, file in pairs(files) do
+		local founded = false
+		local skiped = false
+
+		print("==> testing " .. class)
+
+		for classB, fileB in pairs(files) do
+			if class == classB then
+				skiped = true
+			elseif not skiped then
+				print(
+					"==> "
+						.. class
+						.. " -- "
+						.. classB
+						.. " founded "
+						.. tostring(founded)
+						.. " skiped "
+						.. tostring(skiped)
+				)
+				local fileContent = readFile(fileB)
+
+				if skipFileByContent(classB, fileContent) then
+					skiped = true
+				elseif fileContent:find(class) then
+					founded = true
+				end
+			end
+		end
+
+		if not skiped and not founded then
+			print("=> Not used " .. file)
+		end
+	end
 end
 
 vim.api.nvim_create_user_command("UnusedJava", Unused.javaClass, {})
